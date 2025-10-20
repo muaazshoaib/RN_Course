cbfedac29082f6e3c1b5dda5fa95688d1d0b7908
muaazshoaib <muaazshoaib1670@gmail.com>
2025-10-20 11:17:24 +0500

    React Refresher => Currently at: 00:46:30

--- react_refresher/src/App.jsx
+++ react_refresher/src/App.jsx
@@ -1,41 +1,57 @@
-// React Refresher => Currently at: 00:33:00
+// // React Refresher => Currently at: 00:46:30
 
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// import { useEffect, useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
 
-// // Class Components
-// class ClassComponents extends React.Component {
-//   render( ) {
-//     return <h2>Class Component</h2>
-//   }
-// }
+// // // Class Components
+// // class ClassComponents extends React.Component {
+// //   render( ) {
+// //     return <h2>Class Component</h2>
+// //   }
+// // }
 
-const hasLiked = true;
+// const hasLiked = true;
 
-const Card = ({ title }) => {
-  const [hasLiked, setHasLiked] = useState(false);
+// const Card = ({ title }) => {
+//   const [count, setCount] = useState(0);
 
-  return (
-    <div className="card">
-      <h2>{title}</h2>
+//   const [hasLiked, setHasLiked] = useState(false);
 
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "❤️" : "🤍"}
-      </button>
-    </div>
-  );
-};
+//   useEffect(() => {
+//     console.log(`${title} has been liked: ${hasLiked}`);
+//   }, [hasLiked]);
+
+//   return (
+//     <div className="card" onClick={() => setCount(count + 1)}>
+//       <h2>
+//         {title} <br /> {count || null}
+//       </h2>
+
+//       <button onClick={() => setHasLiked(!hasLiked)}>
+//         {hasLiked ? "❤️" : "🤍"}
+//       </button>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   return (
+//     <div className="card-container">
+//       <Card title="Star Wars" rating={5} isCool={true} hasLiked={hasLiked} />
+//       <Card title="Avatar" />
+//       <Card title="The Lion King" />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import { React } from "react";
 
 const App = () => {
-  return (
-    <div className="card-container">
-      <Card title="Star Wars" rating={5} isCool={true} hasLiked={hasLiked} />
-      <Card title="Avatar" />
-      <Card title="The Lion King" />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
