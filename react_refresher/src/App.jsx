1c1bef42ff8d96747a634ecbcc442aa5e21a3cd4
muaazshoaib <muaazshoaib1670@gmail.com>
2025-10-20 10:35:03 +0500

    React Refresher => Currently at: 00:33:00

---
+++ react_refresher/src/App.jsx
@@ -0,0 +1,41 @@
+// React Refresher => Currently at: 00:33:00
+
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+// // Class Components
+// class ClassComponents extends React.Component {
+//   render( ) {
+//     return <h2>Class Component</h2>
+//   }
+// }
+
+const hasLiked = true;
+
+const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "❤️" : "🤍"}
+      </button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="card-container">
+      <Card title="Star Wars" rating={5} isCool={true} hasLiked={hasLiked} />
+      <Card title="Avatar" />
+      <Card title="The Lion King" />
+    </div>
+  );
+};
+
+export default App;
